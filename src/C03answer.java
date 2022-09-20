80fcad3bf6be8db466a8e30c60958bfd21294928
nokme76 <power4430@naver.com>
2022-09-20 22:36:39 +0900

    숙제

--- src/C03answer.java
+++ src/C03answer.java
@@ -5,21 +5,23 @@ public class C03answer {
 
         Scanner scan = new Scanner(System.in);
 
+
+        while (true)
+        {
         System.out.println("Up & Down 게임입니다. 숨겨진 수를 맞추어 보세요");
         System.out.println("0-99");
-        int goal = 68;
+        int goal = (int)(Math.random() * 98 ) + 1 ;
+//        int goal = 55;
         int high = 99;
         int low = 0;
-        int cnt = 0;
         int no = 1;
-        boolean y;
+
 
         System.out.print(no + ">>" );
         int num = scan.nextInt();
 
-        while (num != goal)
+        while (goal != num)
         {
-
             if(num > goal)
             {
                 System.out.println("더 낮게");
@@ -29,18 +31,36 @@ public class C03answer {
             {
                 System.out.println("더 높게");
                 low = num;
-
             }
-
             no++;
             System.out.println(low+"-"+high);
             System.out.print(no + ">>" );
             num = scan.nextInt();
         }
 
-        System.out.println("맞았습니다.");
-        System.out.print("다시 하시겠습니까?(y/n)>> ");
-        y = scan.hasNext();
+
+
+                System.out.println("맞았습니다.");
+                System.out.print("다시 하시겠습니까?(y/n)>> ");
+                String a ;
+                a = scan.next();
+                while (true)
+                {
+                    if (a.equals("y") || (a.equals("n")))
+
+                    break;
+                    System.out.println("잘못 입력하셨습니다.  y/n을 입력해주세요");
+                    a = scan.next();
+                }
+
+                if (a.equals("n"))
+                {
+                    break;
+                }
+
 
     }
 }
+}
+
+
---
+++ src/P1answer.java
@@ -0,0 +1,18 @@
+public class P1answer {
+    public static void main(String [] args){
+
+        int i= 0, sum= 0;
+        while (i < 10)
+        {
+            i = i + 2;
+            sum += i;
+        }
+        System.out.println(sum);;
+
+    }
+}
+
+// 문제1) 무엇을 계산하는 프로그램이며, 실행 결과는?
+// 2의 배수 덧셈 , 30
+// 문제2)
+// i == 10 , i%2==1
